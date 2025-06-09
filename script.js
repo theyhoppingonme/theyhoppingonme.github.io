290610ffe7b41e8218b0b176d5bf58b176cd615a
theyhoppingonme <theyhoppingonme@gmail.com>
2025-06-09 17:46:22 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 setInterval(function() {
 console.clear();
 }, 1000);
+  window.addEventListener('click', () => {
+    const audio = document.getElementById('bgm');
+    audio.play();
+  }, { once: true });
