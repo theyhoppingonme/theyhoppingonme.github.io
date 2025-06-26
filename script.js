c72f6e704cb30224df55e574545d28ffad1b1f20
theyhoppingonme <theyhoppingonme@gmail.com>
2025-06-26 06:31:16 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-  window.addEventListener('click', () => {
+     const container = document.querySelector('.container');
+
+	if (container) {
+	  const backCirclesDiv = document.createElement('div');
+  	backCirclesDiv.className = 'backcircles';
+
+ 	 const ul = document.createElement('ul');
+	  for (let i = 0; i < 10; i++) {
+ 	   const li = document.createElement('li');
+ 	   ul.appendChild(li);
+ 	 }
+
+ 	 backCirclesDiv.appendChild(ul);
+ 	 container.appendChild(backCirclesDiv);
+	}
+window.addEventListener('click', () => {
     const audio = document.getElementById('bgm');
     audio.play();
   }, { once: true });
